2d7c8e60603ed6f2aa7cff545f8e61eac2570d95
AzharMoosa <azhar.moosa8@gmail.com>
2021-06-20 14:25:18 +0100

    MongoDB Connected

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connect Database
+connectDB();
 
-app.listen(PORT, () => console.log("Welcome To Fitness Application Backend"));
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Welcome To The Fitness Application API"));
