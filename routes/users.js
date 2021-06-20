4574016c70e5e9d8d81203ed81f0ddb4a7ad8b08
AzharMoosa <azhar.moosa8@gmail.com>
2021-06-20 14:42:53 +0100

    Register Validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
 
 // @route       POST api/users
 // @desc        Registers New User
 // @access      Public
-router.post("/", (req, res) => {
-  res.send("Registers User");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is Required").not().isEmpty(),
+    check("email", "Email Is Not Valid").isEmail(),
+    check("password", "Password Must Be 8 or More Characters").isLength({
+      min: 8,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Middleware
+app.use(express.json({ extended: false }));
+
 // Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
