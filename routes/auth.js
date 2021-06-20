b6c9635e752d8005924e271fe8391b77a8a16d6c
AzharMoosa <azhar.moosa8@gmail.com>
2021-06-20 15:08:54 +0100

    User Login

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // @route       GET api/auth
 // @desc        Gets Current User
@@ -11,8 +15,56 @@ router.get("/", (req, res) => {
 // @route       POST api/auth
 // @desc        Login
 // @access      Public
-router.post("/", (req, res) => {
-  res.send("Login");
-});
+router.post(
+  "/",
+  [
+    check("email", "Email Is Not Valid").isEmail(),
+    check("password", "Password Is Required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Email Or Password Not Valid" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        return res.status(400).json({ msg: "Email Or Password Not Valid" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 31600000,
+        },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("API Error");
+    }
+  }
+);
 
 module.exports = router;
