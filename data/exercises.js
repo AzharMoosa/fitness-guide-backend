1a2a3b06bba0766e79c6f6e444cc68a6898cc4c8
AzharMoosa <azhar.moosa8@gmail.com>
2021-07-22 15:06:10 +0100

    Exercise Seeder

--- data/exercises.js
+++ data/exercises.js
@@ -1,10 +1,15 @@
+import armExercises from "./exercise_list/arm_exercises.js";
+import shoulderExercises from "./exercise_list/shoulder_exercises.js";
+import chestExercises from "./exercise_list/chest_exercises.js";
+import coreExercises from "./exercise_list/core_exercises.js";
+import legExercises from "./exercise_list/leg_exercises.js";
+
 const exercises = [
-  {
-    name: "Hammer Curls",
-    type: "Arm Exercises",
-    description: "",
-    info: "",
-  },
+  ...armExercises,
+  ...shoulderExercises,
+  ...chestExercises,
+  ...coreExercises,
+  ...legExercises,
 ];
 
 export default exercises;
