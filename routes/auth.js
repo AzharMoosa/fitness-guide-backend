3d7060e7f509dace941b9169b5b98ccbed67aa84
AzharMoosa <azhar.moosa8@gmail.com>
2021-07-25 11:04:08 +0100

    Auth GET Route

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ import generateToken from "../utils/generateToken.js";
 // @access      Private
 router.get("/", auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select("-password");
+    const user = await User.findById(req.user._id).select("-password");
     res.json(user);
   } catch (err) {
     console.error(err.message);
