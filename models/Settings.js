95e5fa4f2fa4033893af2e2c93874dcdd66c5db7
AzharMoosa <azhar.moosa8@gmail.com>
2021-07-26 10:18:29 +0100

    Settings Model

--- models/Settings.js
+++ models/Settings.js
@@ -1,10 +1,36 @@
 import mongoose from "mongoose";
 
+const HealthInformationSchema = mongoose.Schema({
+  date_of_birth: {
+    type: Date,
+  },
+  gender: {
+    type: String,
+  },
+  height: {
+    type: String,
+  },
+  weight: {
+    type: Number,
+  },
+});
+
+const ChatSettingsSchema = mongoose.Schema({
+  custom_name: {
+    type: String,
+  },
+  isVisible: {
+    type: Boolean,
+  },
+});
+
 const SettingsSchema = mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "users",
   },
+  health_information: [HealthInformationSchema],
+  chat_settings: [ChatSettingsSchema],
 });
 
 const Settings = mongoose.model("settings", SettingsSchema);
