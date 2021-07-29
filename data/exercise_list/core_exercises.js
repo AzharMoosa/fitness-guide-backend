0366b2557b75fe74fa17fe3e82cb1a2bc9485aa2
AzharMoosa <azhar.moosa8@gmail.com>
2021-07-29 10:47:30 +0100

    Core Exercises

--- data/exercise_list/core_exercises.js
+++ data/exercise_list/core_exercises.js
@@ -1,19 +1,13 @@
 const coreExercises = [
   {
-    name: "2 Handed Kettlebell Swing",
-    type: "Core Exercises",
-    description: "Two Handed Russian Style Kettlebell swing",
-    info: "",
-  },
-  {
     name: "Barbell Ab Rollout",
     type: "Core Exercises",
     description:
-      "Place a barbell on the floor at your feet. Bending at the waist grip the barbell with a shoulder with overhand grip.With a slow controlled motion roll the bar out so that your back is straight.Roll back up raising your hips and butt as you return to the starting position.",
+      "Place a barbell on the floor at your feet. Bending at the waist, grip the barbell with a shoulder-width with overhand grip. With a slow controlled motion roll the bar out so that your back is straight. Roll back up raising your hips and butt as you return to the starting position.",
     info: "",
   },
   {
-    name: "Cable Woodchoppers",
+    name: "Cable Woodchopper",
     type: "Core Exercises",
     description:
       "Set cable pulley slightly lower than chest height. Keep body facing forward with hips stable. Grab the pulley handle fully extend your arms and bring your arms forward and across your body. Hold for 1 second at the end of the movement and slowly return to starting position.",
@@ -23,21 +17,21 @@ const coreExercises = [
     name: "Crunches",
     type: "Core Exercises",
     description:
-      "Lay down on your back a soft surface the feet are on the floor. Ask a partner or use some other help barbell etc. to keep them fixed your hands are behind your head. From this position move your upper body up till your head or elbows touch your knees. Do this movement by rolling up your back.",
+      "Lay down on your back on a soft surface and ensure that your feet are on the floor. Ask a partner or use some other help (barbell) to keep them fixed your hands are behind your head. From this position move your upper body up till your head or elbows touch your knees. Do this movement by rolling up your back.",
     info: "",
   },
   {
     name: "Crunches on Machine",
     type: "Core Exercises",
     description:
-      "The procedure is very similar as for regular crunches only with the additional weight of the machine. Sit on the machine put both feet firmly on the ground. Grab the to the weights cables etc. and do a rolling motion forwards the spine should ideally lose touch vertebra by vertebra. Slowly return to the starting position. ",
+      "The procedure is very similar as for regular crunches only with the additional weight of the machine. Sit on the machine and put both feet firmly on the ground. Do a rolling motion forwards. Slowly return to the starting position. ",
     info: "",
   },
   {
     name: "Crunches With Cable",
     type: "Core Exercises",
     description:
-      "Take the cable on your hands and hold it next to your temples. Knee down and hold your upper body straight and bend forward. Go down with a fast movement rolling your back in your ellbows point to your knees. Once down go slowly back to the initial position.",
+      "Take the cable on your hands and hold it next to your temples. Kneel down and hold your upper body straight and bend forward. Go down with a fast movement rolling your back until your elbows point to your knees. Once down, go slowly back to the initial position.",
     info: "",
   },
   {
@@ -51,56 +45,56 @@ const coreExercises = [
     name: "Deadbug",
     type: "Core Exercises",
     description:
-      "Lie on your back with your hips and knees bent to 90. Raise both arms toward the ceiling. Pull your lower back to the floor to eliminate the gap. Start by pressing one leg out and tapping the heel to the floor. As you extend one leg exhale as much as you can keeping your lower back glued to the floor Dunham says. When you cant exhale any more pull your knee back to the starting position. Make this more difficult by holding weight in your hands or by lowering opposite arm and leg.",
+      "Lie on your back with your hips and knees bent to 90. Raise both arms toward the ceiling. Pull your lower back to the floor to eliminate the gap. Start by pressing one leg out and tapping the heel to the floor. As you extend one leg exhale as much as you can keeping your lower back glued to the floor. When you can't exhale any more pull your knee back to the starting position. Make this more difficult by holding weight in your hands or by lowering opposite arm and leg.",
     info: "",
   },
   {
     name: "Flutter Kicks",
     type: "Core Exercises",
     description:
-      "- Laying on the back lift your straightened legs from the ground at a 45 degree angle. -As your Left foot travels downward and nearly touches the floor your Right foot should seek to reach a 90 degree angle or as close to one as possible.-Bring your R foot down until it nearly touches the floor and bring your L foot upwards. Maintain leg rigidity throughout the exercise. Your head should stay off the ground supported by tightened upper abdominals.-L up R down L down R up x2 v v v v  1 rep-Primarily works the Rectus Abdominus the hip flexors and the lower back. Secondarily works the Obliques. Emphasis placed on the lower quadrant of the abs.",
+      "Laying on the back lift your straightened legs from the ground at a 45 degree angle. As your left foot travels downward and nearly touches the floor your right foot should seek to reach a 90 degree angle or as close to one as possible. Bring your right foot down until it nearly touches the floor and bring your left foot upwards. Maintain leg rigidity throughout the exercise. Your head should stay off the ground supported by tightened upper abdominal.",
     info: "",
   },
   {
     name: "Full Sit Outs",
     type: "Core Exercises",
     description:
-      "A Get in high plank position on your hands and toes.B Shift your weight to your left hand as you turn your body to the right bend your right leg behind you and extend your right arm up. Return to the center and repeat on the opposite side. Continue alternating sides.Make it easierDont raise your arm after you bend your leg behind you.Make it harderBalance with your arm and leg extended for two counts.",
+      "Get in a high plank position on your hands and toes. Shift your weight to your left hand, as you turn your body to the right, bend your right leg behind you and extend your right arm up. Return to the center and repeat on the opposite side. Continue alternating sides.",
     info: "",
   },
   {
     name: "Hanging Leg Raises",
     type: "Core Exercises",
     description:
-      "Hanging from bar or straps bring legs up with knees extended or flexed",
+      "Hanging from bar or straps, bring legs up with knees extended or flexed",
     info: "",
   },
   {
     name: "Hollow Hold",
     type: "Core Exercises",
     description:
-      "Get on a mat and lie on your back. Contract your abs stretch your raise and legs and raise them your head and shoulders are also be raised. Make sure your lower back remains in contact with the mat.",
+      "Get on a mat and lie on your back. Contract your abs stretch your raise and legs and raise them. Head and shoulders are also be raised. Make sure your lower back remains in contact with the mat.",
     info: "",
   },
   {
     name: "Incline Plank With Alternate Floor Touch",
     type: "Core Exercises",
     description:
-      "Perform the plank with legs elevated feet on a gymball. Once stabilised slowly move one foot sideways off the ball then make it touch the floor then come back to starting position. Alternate with the other foot. This is a core exercise.",
+      "Perform the plank with legs elevated feet on a gymball. Once stabilized slowly move one foot sideways off the ball then make it touch the floor then come back to starting position. Alternate with the other foot.",
     info: "",
   },
   {
     name: "Leg Raises, Lying",
     type: "Core Exercises",
     description:
-      "Lay down on a bench and hold onto the recliner with your hands to keep you stable. Hold your legs straight and lift them till they make an angle of about 45. Make a short pause of 1 sec. and go slowly down to the initial position. To increase the intensity you can make a longer pause of 7 sec. every 5th time.",
+      "Lay down on a bench and hold onto the recliner with your hands to keep you stable. Hold your legs straight and lift them till they make an angle of about 45. Make a short pause of 1 sec. Go slowly down to the initial position.",
     info: "",
   },
   {
     name: "Leg Raises, Standing",
     type: "Core Exercises",
     description:
-      "Put your forearms on the pads on the leg raise machine the body is hanging freely. Lift now your legs with a fast movement as high as you can make a short pause of 1sec at the top and bring them down again. Make sure that during the exercise your body does not swing only the legs should move.",
+      "Put your forearms on the pads on the leg raise machine with the body hanging freely. Lift now your legs with a fast movement as high as you can. Make a short pause of 1sec at the top and bring them down again. Make sure that during the exercise your body does not swing only the legs should move.",
     info: "",
   },
   {
@@ -113,88 +107,56 @@ const coreExercises = [
     name: "Negative Crunches",
     type: "Core Exercises",
     description:
-      "Sit yourself on the decline bench and fix your legs. Cross your arms over the chest and bring with a rolling movement your upper body up go now without a pause and with a slow movement down again. Dont let your head move during the exercise.",
-    info: "",
-  },
-  {
-    name: "Overhand Cable Curl",
-    type: "Core Exercises",
-    description:
-      "Hands at shoulder height curl arms in toward head then back out.",
+      "Sit yourself on the decline bench and fix your legs. Cross your arms over the chest and bring with a rolling movement your upper body up. Go now without a pause and with a slow movement down again. Don't let your head move during the exercise.",
     info: "",
   },
   {
     name: "Plank",
     type: "Core Exercises",
     description:
-      "Get into a position on the floor supporting your weight on your forearms and toes. Arms are bent and directly below the shoulder. Keep your body straight at all times and hold this position as long as possible. To increase difficulty an arm or leg can be raised while performing this exercise. ",
-    info: "",
-  },
-  {
-    name: "Reverse Plank",
-    type: "Core Exercises",
-    description: "Plank with stomach towards ceiling",
-    info: "",
-  },
-  {
-    name: "Roman Chair",
-    type: "Core Exercises",
-    description: "Crunches on roman chair",
+      "Get into a position on the floor supporting your weight on your forearms and toes. Arms are bent and directly below the shoulder. Keep your body straight at all times and hold this position as long as possible.",
     info: "",
   },
   {
     name: "Russian Twists",
     type: "Core Exercises",
     description:
-      "To perform the Russian twist one sits on the floor and bends both knees while feet are be kept together and held slightly above the ground or put under a stable surface. Ideally the torso is kept straight with the back kept off the ground at a 45 degree angle with arms held together away from the body in a straight fashion and hands kept locked together like a ball or one can hold a weight to increase the difficulty. Next the arms should be swung from one side to another in a twisting motion with each swing to a side counting as one repetition. The slower one moves the arms from side to side the harder the exercise becomes working the abdomen that much better. When moving ones arms during the exercise it is crucial to not stop between repetitions or else one will lose the effect of working the abdomen. As with all exercise consistent breathing in and out during the exercise is important as one must ensure proper oxygen flow.",
-    info: "",
-  },
-  {
-    name: "Scissors",
-    type: "Core Exercises",
-    description:
-      "Scissors is an abdominal exercise that strengthens the transverse abdominals helping flatten your belly and strengthen your entire core. Scissors is not only a core strength move but it is also a great stretch for your hamstrings and your lower back. Everyone is looking for new ways to work the core toflatten the bellyand to improve flexibility. If you learn how to do Scissors you will get everything rolled together in one move.",
+      "Bends both knees while feet are be kept together and held slightly above the ground or put under a stable surface. Ideally the torso is kept straight with the back kept off the ground at a 45 degree angle with arms held together away from the body in a straight fashion and hands kept locked together like a ball. Next the arms should be swung from one side to another in a twisting motion with each swing to a side counting as one repetition. The slower one moves the arms from side to side the harder the exercise becomes working the abdomen that much better. When moving ones arms during the exercise it is crucial to not stop between repetitions or else one will lose the effect of working the abdomen.",
     info: "",
   },
   {
     name: "Side Crunch",
     type: "Core Exercises",
     description:
-      "Hold weight in one hand. Bend side ways to the knee. Pull upo to upright position using your obliquus.",
+      "Hold weight in one hand. Bend side ways to the knee. Pull up to a upright position using your obliques.",
     info: "",
   },
   {
     name: "Side Plank",
     type: "Core Exercises",
     description:
-      "Works your obliques and helps stabilize your spine. Lie on your side and support your body between your forearm and knee to your feet.",
+      "Lie on your side and support your body between your forearm and knee to your feet.",
     info: "",
   },
   {
     name: "Sit-ups",
     type: "Core Exercises",
     description:
-      "Sit on a mat your calves are resting on a bench the knees make a right angle. Hold your hands behind your neck. Go now up with a rolling movement of your back you should feel how the individual vertebrae lose contact with the mat. At the highest point contract your abs as much as you can and hold there for 2 sec. Go now down unrolling your back. ",
+      "Sit on a mat, with your calves resting on a bench and the knees make a right angle. Hold your hands behind your neck. Go now up with a rolling movement of your back you should feel how the individual vertebrae lose contact with the mat. At the highest point contract your abs as much as you can and hold there for 2 sec. Go now down unrolling your back. ",
     info: "",
   },
   {
     name: "Splinter Sit-ups",
     type: "Core Exercises",
     description:
-      "Lie on your back with your legs straight and arms at your sides keeping your elbows bent at 90 degrees. As you sit up twist your upper body to the left and bring your left knee toward your right elbow while you swing your left arm back. Lower your body to the starting position and repeat to your right. Thats 1 rep.",
-    info: "",
-  },
-  {
-    name: "Trunk Rotation With Cable ",
-    type: "Core Exercises",
-    description: "Seated trunk rotation with cable ",
+      "Lie on your back with your legs straight and arms at your sides keeping your elbows bent at 90 degrees. As you sit up twist your upper body to the left and bring your left knee toward your right elbow while you swing your left arm back. Lower your body to the starting position and repeat to your right.",
     info: "",
   },
   {
     name: "Turkish Get-Up",
     type: "Core Exercises",
     description:
-      "Starting on back move to the standing position with dumbbell in one hand. Switch hands between reps.",
+      "Starting on your back, move to the standing position with dumbbell in one hand. Switch hands between reps.",
     info: "",
   },
 ];
